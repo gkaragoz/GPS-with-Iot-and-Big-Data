48dee3d9e1cbfcf08b06a0ad4f5d57a23fb7d9b0
gkaragoz <gkaragoz96@gmail.com>
2017-11-28 18:49:59 +0300

    Ingregrate api/gps getGps function

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+Gps = require('./models/gps');
+
 //Connect to Mongoose
 mongoose.connect('mongodb://localhost/gpsdb', {
   useMongoClient: true,
@@ -12,6 +14,15 @@ var db = mongoose.connection;
 
 app.get('/', function(req, res){
   res.send('Please use /api/gps');
+});
+
+app.get('/api/gps', function(req, res){
+  Gps.getGps(function(err, gps){
+    if(err){
+      throw err;
+    }
+    res.json(gps);
+  });
 });
 
 app.listen(3000);
