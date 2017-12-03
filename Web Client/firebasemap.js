ed12f21d8040d4278bbf0201f8f2774795831dfe
gkaragoz <gkaragoz96@gmail.com>
2017-12-03 17:06:35 +0300

    Add makeInfoBox function to show something at top of map on screen

--- Web Client/firebasemap.js
+++ Web Client/firebasemap.js
@@ -11,4 +11,33 @@ function initMap() {
     }],
     disableDoubleClickZoom: true
   });
+
+  // Create the DIV to hold the control and call the makeInfoBox() constructor
+  // passing in this DIV.
+  var infoBoxDiv = document.createElement('div');
+  var infoBox = new makeInfoBox(infoBoxDiv, map);
+  infoBoxDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(infoBoxDiv);
 }
+
+function makeInfoBox(controlDiv, map) {
+   // Set CSS for the control border.
+   var controlUI = document.createElement('div');
+   controlUI.style.boxShadow = 'rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px';
+   controlUI.style.backgroundColor = '#fff';
+   controlUI.style.border = '2px solid #fff';
+   controlUI.style.borderRadius = '2px';
+   controlUI.style.marginBottom = '22px';
+   controlUI.style.marginTop = '10px';
+   controlUI.style.textAlign = 'center';
+   controlDiv.appendChild(controlUI);
+
+   // Set CSS for the control interior.
+   var controlText = document.createElement('div');
+   controlText.style.color = 'rgb(25,25,25)';
+   controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+   controlText.style.fontSize = '100%';
+   controlText.style.padding = '6px';
+   controlText.innerText = 'The map shows all clicks made in the last 10 minutes.';
+   controlUI.appendChild(controlText);
+ }
