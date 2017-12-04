11b11a74439a436c80d7959211f233975ee518d5
gkaragoz <gkaragoz96@gmail.com>
2017-12-04 18:39:53 +0300

    Add sample contents.

--- Web Client/main.js
+++ Web Client/main.js
@@ -3,5 +3,24 @@ function initApplication(){
   initFirebase();
   initCustomUIManager();
 
+
+  var marker = [
+    {
+      coords:{lat:40.748378,lng:30.418285}
+    },
+    {
+      coords:{lat:40.743817,lng:30.338253},
+      iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+      content:'<h1>Sakarya University</h1>'
+    },
+    {
+      coords:{lat:40.756025,lng:30.368216}
+    }
+  ];
+
+  addMapMarker(marker[0]);
+  addMapMarker(marker[1]);
+  addMapMarker(marker[2]);
+
   getFirebaseUserData(1);
 }
