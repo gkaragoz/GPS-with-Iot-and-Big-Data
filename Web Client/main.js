675cf5d4b1b1b4d8f6e5dc418c17bf81957bdb24
gkaragoz <gkaragoz96@gmail.com>
2017-12-04 17:17:30 +0300

    Delete web client click events and unnecessary functions

--- Web Client/main.js
+++ Web Client/main.js
@@ -1,40 +1,13 @@
 function initMap(){
   //Map options
-  //Sakarya 40.6940° N, 30.4358° E
+  //Sakarya 40.775384° N, 30.366367° E
   var options = {
-    zoom:10,
-    center:{lat:40.6940,lng:30.4358}
+    zoom:12,
+    center:{lat:40.775384,lng:30.366367}
   }
 
   //New map
   var map = new google.maps.Map(document.getElementById('map'), options);
-
-  //Listen for click on map
-  google.maps.event.addListener(map, 'click',
-  function(event){
-    //Add marker
-    addMarker({coords:event.latLng});
-  });
-
-  //Array of markers
-  var markers = [
-    {
-      coords:{lat:40.743817,lng:30.338253},
-      iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-      content:'<h1>Sakarya University</h1>'
-    },
-    {
-      coords:{lat:40.553817,lng:30.338253}
-    },
-    {
-      coords:{lat:40.853817,lng:30.455253}
-    }
-  ];
-
-  //Loop through markers
-  for(var i=0; i<markers.length; i++){
-    addMarker(markers[i]);
-  }
 
   //Add marker function
   function addMarker(props){
@@ -60,5 +33,80 @@ function initMap(){
         infoWindow.open(map, marker);
       });
     }
+  }
+
+  initFirebase();
+}
+
+function initFirebase(){
+  var data = {
+    timestamp: 148420310123,
+    lat: 40.775384,
+    lng: 30.366367
+  }
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCcNAWv3tggQHNt1QN3-X-vjB_uQBC3R8",
+    authDomain: "iot-gps-for-sau.firebaseapp.com",
+    databaseURL: "https://iot-gps-for-sau.firebaseio.com",
+    projectId: "iot-gps-for-sau",
+    storageBucket: "iot-gps-for-sau.appspot.com",
+    messagingSenderId: "195237350046"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the auth service
+  var auth = firebase.auth();
+
+  firebase.auth().signInAnonymously().catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      console.log("[info] User has been logged in: " + uid);
+      data.sender = uid;
+
+      writeUserData(data);
+      getUserData(1);
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+    // ...
+  });
+
+  function getUserData(postId){
+    var databaseRef = firebase.database().ref();
+    databaseRef.on('value', function(snapshot) {
+      var data = JSON.stringify(snapshot.val());
+      var user01 = JSON.parse(data);
+
+      console.log(JSON.stringify(data));
+    });
+    console.log("[info] User data has been got!");
+  }
+
+  function writeUserData(data) {
+    console.log("[info] " + JSON.stringify(data) + " will write!");
+
+    // Get a reference to the database service
+    var databaseRef = firebase.database().ref();
+    var locationsRef = databaseRef.child('app/locations/' + data.sender);
+    var newDataRef = locationsRef.push();
+    newDataRef.set({
+      timestamp: data.timestamp,
+      lat: data.lat,
+      lng: data.lng
+    });
+    console.log("[info] Data has been written!");
   }
 }
